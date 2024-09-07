e2a917fccadde9ad8f70d10f57f7ab200b92e877
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-09-07 13:18:55 +0200

    Initial commit with project setup

---
+++ src/main/java/com/krzysztof_kolodziej/project/smart_money/SmartMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.krzysztof_kolodziej.project.smart_money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krzysztof_kolodziej/project/smart_money/SmartMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzysztof_kolodziej.project.smart_money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
